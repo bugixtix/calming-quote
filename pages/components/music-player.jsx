a5fb2642d34b50246a404ab46d945d6f11933246
Housam_ <ixtix_bug@hotmail.com>
2025-02-02 17:38:10 +0100

    Update: Component

--- pages/components/music-player.jsx
+++ pages/components/music-player.jsx
@@ -8,53 +8,86 @@ import { MdOutlinePlayCircleFilled as PlayFi} from "react-icons/md";
 import { MdPlayCircleOutline as PlayOl } from "react-icons/md";
 import Image from "next/image";
 import Lofi from '@/public/lofi.jpg'
-// import { ProgressBar } from "react-bootstrap";
-// import lofi from '@/public/mp3/good_night_lofi.mp3'
-// const lofi = require('@/public/mp3/good_night_lofi.mp3')
-import useSound from 'use-sound'
-import { audio } from "motion/react-client";
-// import url from '@/public/mp3/good_night_lofi.mp3'
-// import { audio } from "motion/react-client";
+import MP3_DATA from '@/pages/mp3_list.json'
 
 export const MusicPlayer = () =>{
-    const pfad = '/mp3/good_night_lofi.mp3'
+    // const pfad = '/mp3/good_night_lofi.mp3'
+    const [track, setTrack] = useState(MP3_DATA[1])
+    const [playing, setPlaying] = useState(false)
     const [currentTime, setCurrentTime] = useState(0)
+    const [maxTime, setMaxTime] = useState(0)
     const soundRef = useRef()
 
     
     useEffect(()=>{
-        soundRef.current = new Audio(pfad)
-    },[])
+        soundRef.current = new Audio(track.track_url)
+        // new code from chatgpt
+        const audio = soundRef.current;
+        const updateTime=()=>setCurrentTime(audio.currentTime.toFixed())
+        const updateDuration=()=>setMaxTime(audio.duration.toFixed())
+        const updatePath=()=>setTrack(MP3_DATA[2])
+        
+        audio.addEventListener("timeupdate", updateTime)
+        audio.addEventListener("loadedmetadata", updateDuration)
+        audio.addEventListener("ended", updatePath)
+
+        return()=> {
+            audio.removeEventListener("timeupdate", updateTime)
+            audio.removeEventListener("loadedmetadata", updateDuration)
+            audio.removeEventListener("ended", updatePath)
+        }
+        // 
+    },[track])
     const HandleSound =()=>{
-        soundRef.current.play()
+        if(!playing) 
+        {soundRef.current.play()
+         setPlaying(true)
+        }else{
+            soundRef.current.pause()
+            setPlaying(false)
+        }
+        console.log(soundRef.current.duration)
     }
     const updateTime = () =>{
-    
+    }
+    const formatTime = seconds =>{
+        let mins = 0
+        let secs = 0
+        if(!isNaN(seconds)){
+            mins = Math.floor(seconds/60).toString().padStart(2,'0');
+            secs = (seconds%60).toString().padStart(2,'0')
+        }else{
+            mins = '01'
+            secs = '00'
+        }
+        return `${mins}:${secs}`
     }
    return(
     <div className="MusicPlayer">
         <div className="MusicPlayerBody">
             <div className="player--img">
-                <Image src={Lofi} className="image"/>
+                <Image src={track.track_cover || '/lofi.jpg'} width={100} height={100} className="image"/>
                 {/* <span>img here</span> */}
             </div>
             <div className="player--content">
                 <div className="player--info">
-                    <h2> Unknown Track </h2>
+                    <h2> {track?.track_name || "Unknown Track"} </h2>
                     {/* <p>Author</p> */}
                 </div>
                 <div className="player--options">
-                    <ProgressBar currentValue={50}/>
+                    <ProgressBar currentValue={currentTime} maxValue={soundRef.current?.duration||1}/>
                     <div className="audio--length">
-                        <p className="start-value">02:02</p>
-                        <p className="final-value">04:04</p>
+                        <p className="start-value">{formatTime(currentTime)}</p>
+                        <p className="final-value">{formatTime(maxTime)}</p>
                     </div>
                 </div>
             </div>
             
             <div className="player--button">
                 <button className="play-button" onClick={HandleSound}>
-                    <PlayOl className="play-icon"/>
+                {
+                    playing ? <PauseOl className="play-icon"/> :  <PlayOl className="play-icon"/>
+                }
                 </button>
             </div>
         </div>
